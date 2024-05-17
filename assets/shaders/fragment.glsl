65a411addc416cb9ee461a5b798e686772631b8e
crossfrog <devcrossfrog@gmail.com>
2024-05-17 04:17:00 -0400

    Alpha test

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -6,5 +6,11 @@ out vec4 fColor;
 uniform sampler2D fTexture;
 
 void main() {
-    fColor = texture(fTexture, fUV);
+    vec4 color = texture(fTexture, fUV);
+
+    if (color.a == 0.0) {
+        discard;
+    }
+
+    fColor = color;
 }
