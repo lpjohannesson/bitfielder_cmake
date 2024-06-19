2042e44b448fbac049edc0ecbe297b5b3d046dc5
crossfrog <devcrossfrog@gmail.com>
2024-06-19 15:15:39 -0400

    OpenGL ES

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -130,8 +130,8 @@ Engine::Engine() : fullscreenAction(input) {
 
 	// Use OpenGL
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 
 	gladLoadGLLoader(SDL_GL_GetProcAddress);
 
